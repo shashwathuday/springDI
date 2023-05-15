6d1087020d716cc7fd4d6615199d4c790a470afe
shashwathuday <46096060+shashwathuday@users.noreply.github.com>
2023-05-15 13:59:23 -0400

    Update tesla.java

--- car/src/main/java/com/example/demo/tesla.java
+++ car/src/main/java/com/example/demo/tesla.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = "prototype")
 
 public class tesla {
 	private int aid;
 	private String aname;
 	private String tech;
-	public int getAid() {
-		return aid;
+	
+	
+	public tesla() {
+		super();
+		System.out.println("object created");
 	}
 	public void setAid(int aid) {
 		this.aid = aid;
